54d27e93625698cb0bfee262df8f0a5e92f64976
Gino Herrera <ginolouisherrera@gmail.com>
2025-12-09 16:03:13 +0800

    Revert "test"
    This reverts commit c9639ebbffe294aeca6037794112970ffe750824.

--- hangman/src/hangman/hangman.java
+++ hangman/src/hangman/hangman.java
@@ -215,7 +215,7 @@ public class hangman extends JFrame {
             mistakes++;
             updateMan();
             if (mistakes >= 6) {
-                JOptionPane.showMessageDialog(this, "Game Over! The word was:  ewan" + secretWord);
+                JOptionPane.showMessageDialog(this, "Game Over! The word was: " + secretWord);
                 resetGame();
             }
         }
@@ -265,5 +265,3 @@ public class hangman extends JFrame {
         new hangman().setVisible(true);
     }
 }
-
-//TESTTTT
